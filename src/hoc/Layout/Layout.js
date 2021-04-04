9ac58f78393f4db3e1fecc8f016e218b705279e8
Alexander Sosov <nookat.develop@gmail.com>
2021-04-04 16:54:08 +0300

    feat: add menu, add drawer

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,16 +1,46 @@
-import React, {Component} from 'react';
-import classes from './Layout.module.css';
+import React, {Component} from 'react'
+import classes from './Layout.module.css'
+import MenuToggle from '../../components/Navigation/MenuToggle'
+import Drawer from '../../components/Navigation/Drawer'
 
 class Layout extends Component {
+  state = {
+    menu: false,
+  }
+
+  toggleMenuHandler = () => {
+    this.setState(prevState => {
+      return {menu: !prevState.menu}
+    })
+  }
+
+  menuCloseHandler = () => {
+    this.setState({
+      menu: false,
+    })
+  }
+
   render() {
+    console.log(this.state.menu)
     return (
       <div className={classes.Layout}>
+
+        <Drawer
+          isOpen={this.state.menu}
+          onClose={this.menuCloseHandler}
+        />
+
+        <MenuToggle
+          onToggle={this.toggleMenuHandler}
+          isOpen={this.state.menu}
+        />
+
         <main>
           {this.props.children}
         </main>
       </div>
-    );
+    )
   }
 }
 
-export default Layout;
+export default Layout
